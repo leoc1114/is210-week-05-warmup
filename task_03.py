bebafe57d0e96b91b33bc29de1ddf6b481c7722e
Wei Chen <wei.chen@spsmail.cuny.edu>
2015-02-27 22:55:40 +0000

    modified task 3

--- task_03.py
+++ task_03.py
@@ -5,5 +5,5 @@
 
 import hamlet
 
-KEYWORD = hamlet.crazy_math(monkeys=84, hours = 200000, typewriters = None,
+KEYWORD = hamlet.crazy_math(monkeys=84, hours=200000, typewriters=None,
                             bananas=48)
